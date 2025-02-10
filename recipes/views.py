b4921d2615f849f893efc9aa7758947dd1a736ee
Victor Renato de Souza <53796509886@>
2025-02-10 15:28:12 -0300

    first_template

--- recipes/views.py
+++ recipes/views.py
@@ -2,16 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('''<!DOCTYPE>
-    <html>
-       <head>
-            <title>Recipes</title>    
-        </head>
-        <body>
-            <h1>HELLO GOOD </h1>
-        </body>
-    </html>
-    ''')
+    return render(request,'home.html')
 
 def contact(request):
     return HttpResponse('ZIPZAP')
